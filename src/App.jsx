d526a6764f389933434c09f7e0c8e8af1c62b8a0
ruchap24 <patilrucha991@gmail.com>
2025-03-03 11:29:28 +0530

    Add routing and loading state to App; enhance Navbar with search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Search from "./components/Search";
+import PlayingVideo from "./components/PlayingVideo";
+import { useAuth } from "./context/AuthProvider";
+import Loading from "./loader/Loading";
 
-//rfce
 function App() {
+  const { loading } = useAuth();
   return (
-    <div><Navbar /></div>
+    <div>
+      {loading && <Loading />}
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/search/:searchQuery" element={<Search />} />
+        <Route path="/video/:id" element={<PlayingVideo />} />
+      </Routes>
+    </div>
   );
 }
 
-export default App
+export default App;
